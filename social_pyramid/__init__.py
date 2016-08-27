d607e04023f219419fdac09b5ad6f511e5235997
Matías Aguirre <matiasaguirre@gmail.com>
2016-08-27 20:45:49 -0300

    Clean up python-social-auth

---
+++ social_pyramid/__init__.py
@@ -0,0 +1,15 @@
+__version__ = '0.0.1'
+
+def includeme(config):
+    from social_core.utils import setting_name
+
+    if config.registry.settings.get(setting_name('TRAILING_SLASH')):
+        extra = '/'
+    else:
+        extra = ''
+
+    config.add_route('social.auth', '/login/{backend}' + extra)
+    config.add_route('social.complete', '/complete/{backend}' + extra)
+    config.add_route('social.disconnect', '/disconnect/{backend}' + extra)
+    config.add_route('social.disconnect_association',
+                     '/disconnect/{backend}/{association_id}' + extra)
